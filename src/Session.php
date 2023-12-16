314a99a5e9440e12e325cda2748e8662f3f25800
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2023-12-16 13:12:20 +0100

    Added static getter and setter into Session class.

--- src/Session.php
+++ src/Session.php
@@ -9,4 +9,11 @@
             session_start();
             $this->id=session_id();
         }
+
+        static function setSession($session, $value){
+            $_SESSION[$session] = $value;
+        }
+        static function getSession($session){
+            return $_SESSION[$session];
+        }
     }
