5e087bc53f887baa1137edf0e4e3da1028772883
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-01-13 23:39:57 +0100

    Change PDO attribute from Warning to Exception.

--- config.php
+++ config.php
@@ -6,6 +6,6 @@
         'connection'=>$_ENV['DB_DRIVER'].':host='.$_ENV['DB_HOST'],
         'dbname'=>$_ENV['DB_NAME'],
         'options'=>[
-            \PDO::ATTR_ERRMODE=>\PDO::ERRMODE_WARNING
+            \PDO::ATTR_ERRMODE=>\PDO::ERRMODE_EXCEPTION
         ]
     ];
