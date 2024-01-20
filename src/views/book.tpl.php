5734f6efb2e52415e2dc8f9443c781950fbcd7ef
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-01-21 00:36:58 +0100

    Added classes to book view.

--- src/views/book.tpl.php
+++ src/views/book.tpl.php
@@ -2,20 +2,26 @@
 include_once 'partials/header.tpl.php';
 ?>
 <body>
+<?php include_once 'partials/nav.tpl.php'; ?>
+    <div class="container-book">
+
     <?php  
         $file = fopen('public/markdown/book_'.$book_id.'.md', 'rb');
-        if($file){
-            while(($line = fgets($file)) !== false){
+        if ($file) {
+            while (($line = fgets($file)) !== false) {
                 if (trim($line) !== '') {
-                    if($line[0] == '#') echo '<h1>'.trim(substr($line, 1)).'</h1>';
-                    else echo '<p>'.trim($line).'</p>';
+                    if ($line[0] == '#') {
+                        echo '<h1>' . trim(substr($line, 1)) . '</h1>';
+                    } else {
+                        echo '<p>' . trim($line) . '</p>';
+                    }
                 }
-                
             }
+            fclose($file);
         }
-        fclose($file);
-        
     ?>
+
+    </div>
 </body>
 
 </html>
